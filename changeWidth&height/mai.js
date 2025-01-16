2e856390d5dfd1a8960e821f82687a105da2314c
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-16 22:43:31 +0530

    js file

---
+++ changeWidth&height/mai.js
@@ -0,0 +1,8 @@
+function size()
+{
+    var h = document.getElementById('height').value;
+    document.getElementById('box').style.height= h+'px';
+    document.write('hellow');
+    var w = document.getElementById('width').value; 
+    document.getElementById('box').style.width= w+'px';
+}
